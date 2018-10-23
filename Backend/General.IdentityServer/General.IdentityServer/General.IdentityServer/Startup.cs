1757eecf9a5c3795c35209b4f7d0b389b4f11e63
Pawel Petrusewitch <shuster.pashka@gmail.com>
2018-10-23 22:10:44 +0300

    *add middleware

--- Backend/General.IdentityServer/General.IdentityServer/General.IdentityServer/Startup.cs
+++ Backend/General.IdentityServer/General.IdentityServer/General.IdentityServer/Startup.cs
@@ -25,7 +25,8 @@ namespace General.IdentityServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
